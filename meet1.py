b794728ce9479f58aa525494679562bab59e0bda
Lalunabel <sandalsandalputus83@gmail.com>
2025-04-11 10:03:43 +0800

    Update meet1.py

--- meet1.py
+++ meet1.py
@@ -1,11 +1,11 @@
-# perbandingan kemungkinan nilai = ture/salah (bolean
+# perbandingan kemungkinan nilai = true/salah (bolean)
 # 1 > 2 =
 # <, >, >=, <=, !=, not
-# a = 24
-# b = 67
+# a = 2 digit nim pertama 
+# b = 2 digit nim terakhir
 
 a = 24
-b = 67
+b = 60
 hasil = a < b
 print(hasil)
 
